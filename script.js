5b6f947b73f20bac61c2598d9f085bd575059433
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-04-17 15:45:29 -0500

    form for adding books

--- script.js
+++ script.js
@@ -59,3 +59,69 @@ function displayBooks() {
     cardContainer.appendChild(row);
   }
 }
+
+// Add event listener to the add book button
+addBookBtn.addEventListener('click', function() {
+  // Display a form to add a new book
+  const form = document.createElement('form');
+
+  // Create input fields for the book properties
+  const titleInput = document.createElement('input');
+  titleInput.type = 'text';
+  titleInput.placeholder = 'Title';
+
+  const authorInput = document.createElement('input');
+  authorInput.type = 'text';
+  authorInput.placeholder = 'Author';
+
+  const pagesInput = document.createElement('input');
+  pagesInput.type = 'number';
+  pagesInput.placeholder = 'Pages';
+
+  const readInput = document.createElement('input');
+  readInput.type = 'checkbox';
+  readInput.id = 'read';
+  
+  const readLabel = document.createElement('label');
+  readLabel.htmlFor = 'read';
+  readLabel.textContent = 'Read';
+
+  // Create a submit button
+  const submitBtn = document.createElement('button');
+  submitBtn.type = 'submit';
+  submitBtn.textContent = 'Add Book';
+
+  // Add the form elements to the form
+  form.appendChild(titleInput);
+  form.appendChild(authorInput);
+  form.appendChild(pagesInput);
+  form.appendChild(readInput);
+  form.appendChild(readLabel);
+  form.appendChild(submitBtn);
+
+  // Add an event listener to the form's submit button
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    // Get the form data
+    const title = titleInput.value;
+    const author = authorInput.value;
+    const pages = pagesInput.value;
+    const read = readInput.checked;
+
+    // Add the new book to the library
+    addBookToLibrary(title, author, pages, read);
+
+    // Clear the form
+    titleInput.value = '';
+    authorInput.value = '';
+    pagesInput.value = '';
+    readInput.checked = false;
+
+    // Display the updated library
+    displayBooks();
+  });
+
+  // Add the form to the card container
+  cardContainer.appendChild(form);
+});
