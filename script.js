96d7a3673f4d8bbc596364f8cb878a2472c547f9
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-04-17 14:36:37 -0500

    variables constructor and add to library

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 // Variables
-
+const addBookBtn = document.querySelector('#addBookBtn');
+const cardContainer = document.querySelector('#cardContainer');
 // library array
 let myLibrary = [];
 
 // book constructor
-function Book(title, author, pages, read) {}
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages
+    this.read = read;
+}
 
 // add book to library
-function addBookToLibrary() {}
+function addBookToLibrary(title, author, pages, read) {
+    let newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+}
+
+// submit button
